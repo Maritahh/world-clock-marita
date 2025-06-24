eff51c9cee78a0fd72a6d785a506edea45dbca01
Marita <mhegertun@gmail.com>
2025-06-24 15:20:02 +0200

    Added basic js to main cities h2
    Also corrected some css and html. And added Moment in html

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+function updateTime() {
+  // los angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Mexico City
+  let mexicoCityElement = document.querySelector("#mexico-city");
+  let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
+  let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
+  let mexicoCityTime = moment().tz("Mexico/General");
+
+  mexicoCityDateElement.innerHTML = mexicoCityTime.format("MMMM Do YYYY");
+  mexicoCityTimeElement.innerHTML = mexicoCityTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
